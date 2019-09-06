b21b20755794133f5a63670553ab6e90c6d0dd10
咸菜 <lplee7830@gmail.com>
2019-09-06 19:53:32 +0800

    sync code

---
+++ helloworld/src/Animal.java
@@ -0,0 +1,13 @@
+/**
+ * @author lipc
+ */
+public class Animal {
+	public static void main(String[] args) {
+		doSomething();
+	}
+
+	private static void doSomething() {
+		String word = "this is from Animal";
+		System.out.println(word.length());
+	}
+}
---
+++ helloworld/src/HelloWord.java
@@ -0,0 +1,15 @@
+/**
+ * @author lipc 啰嗦的 Java 语法规则,要在函数的前面加上 static
+ */
+public class HelloWord {
+
+
+	public static void main(String[] args) {
+		doSomething();
+	}
+
+	private static void doSomething() {
+		System.out.println("this  is form hello world ");
+	}
+}
+
