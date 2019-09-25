e9178c39acd3a4d6183ba3e4c3b2cfc4a5212161
咸菜 <lplee7830@gmail.com>
2019-09-25 22:31:06 +0800

    lesson two data structure of stack

--- helloworld/src/com/company/Main.java
+++ helloworld/src/com/company/Main.java
@@ -3,49 +3,52 @@ package com.company;
 import lesson.two.LessonTwoDataStructureStack;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author lipc
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        byte[] buf = new byte[512];
-        int length = System.in.read(buf);
+	public static void main(String[] args) throws IOException {
+		byte[] buf = new byte[10];
+		int length = System.in.read(buf);
 
-        LessonTwoDataStructureStack s = new LessonTwoDataStructureStack(512);
+		LessonTwoDataStructureStack s = new LessonTwoDataStructureStack(10);
+		System.out.println("length is : " + length);
 
-        for (int i = 0; i < length; i++) {
-            if (buf[i] == '(' || buf[i] == '[' || buf[i] == '{') {
-                s.push(buf[i]);
-            } else if (buf[i] == ')') {
-                if (s.getTop() == '(') {
-                    s.pop();
-                } else {
-                    System.out.println("1 unmatched");
-                    System.exit(1);
-                }
-            } else if (buf[i] == ']') {
-                if (s.getTop() == '[') {
-                    s.pop();
-                } else {
-                    System.out.println("2 unmatched");
-                    System.exit(1);
-                }
-            } else if (buf[i] == '}') {
-                if (s.getTop() == '{') {
-                    s.pop();
-                } else {
-                    System.out.println("3 unmatched");
-                    System.exit(1);
-                }
-            }
-        }
+		for (int i = 0; i < length; i++) {
+			if (buf[i] == '(' || buf[i] == '[' || buf[i] == '{') {
+				s.push(buf[i]);
+				System.out.println(Arrays.toString(s.returnData()));
+			} else if (buf[i] == ')') {
+				if (s.getTop() == '(') {
+					s.pop();
+				} else {
+					System.out.println("1 unmatched");
+					System.exit(1);
+				}
+			} else if (buf[i] == ']') {
+				if (s.getTop() == '[') {
+					s.pop();
+				} else {
+					System.out.println("2 unmatched");
+					System.exit(1);
+				}
+			} else if (buf[i] == '}') {
+				if (s.getTop() == '{') {
+					s.pop();
+				} else {
+					System.out.println("3 unmatched");
+					System.exit(1);
+				}
+			}
+		}
 
-        if (s.isEmpty()) {
-            System.out.println("4 unmatched");
-        } else {
-            System.out.println("all matched");
-        }
-    }
+		if (!s.isEmpty()) {
+			System.out.println("4 unmatched");
+		} else {
+			System.out.println("all matched");
+		}
+	}
 }
