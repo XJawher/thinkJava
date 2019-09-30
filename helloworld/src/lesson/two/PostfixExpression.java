2e8254183cfb3892dbce1284fa42e8805b0bceb0
咸菜 <lplee7830@gmail.com>
2019-09-30 17:26:58 +0800

    lesson two update data structure stack

--- helloworld/src/lesson/two/PostfixExpression.java
+++ helloworld/src/lesson/two/PostfixExpression.java
@@ -1,5 +1,7 @@
 package lesson.two;
 
+import java.util.Arrays;
+
 /**
  * @author lipc
  */
@@ -8,6 +10,7 @@ public class PostfixExpression {
 
 	public void consoleLog(String args) {
 		System.out.println(args);
+		System.out.println(transStringToNumber.length);
 	}
 
 	public void getParameterFromMain(String args) {
@@ -15,43 +18,39 @@ public class PostfixExpression {
 		for (int i = 0; i < args.length(); i++) {
 			// this is means we need calculated previous input data
 			if (args.charAt(i) == '-' || args.charAt(i) == '+' || args.charAt(i) == '*' || args.charAt(i) == '/') {
-				if (transStringToNumber.length != 2) {
-					System.out.println(" 111");
-					System.out.println("parameter input error!!!");
-				} else {
-					System.out.println(" 222");
-					switch (args.charAt(i)) {
-						case '+':
-							System.out.println(" 222 +++ ");
-							result = transStringToNumber[0] + transStringToNumber[1];
-							break;
-						case '-':
-							result = transStringToNumber[0] - transStringToNumber[1];
-							System.out.println(" 222 --- ");
-							break;
-						case '*':
-							System.out.println(" 222 *** ");
-							result = transStringToNumber[0] * transStringToNumber[1];
-							break;
-						case '/':
-							System.out.println(" 222 /// ");
-							result = transStringToNumber[0] / transStringToNumber[1];
-							break;
-						default:
-					}
+				switch (args.charAt(i)) {
+					case '+':
+						System.out.println(" 222 +++ ");
+						result = transStringToNumber[0] + transStringToNumber[1];
+						break;
+					case '-':
+						result = transStringToNumber[0] - transStringToNumber[1];
+						System.out.println(" 222 --- ");
+						break;
+					case '*':
+						System.out.println(" 222 *** ");
+						result = transStringToNumber[0] * transStringToNumber[1];
+						break;
+					case '/':
+						System.out.println(" 222 /// ");
+						result = transStringToNumber[0] / transStringToNumber[1];
+						break;
+					default:
 				}
+				// set the stack empty
+				transStringToNumber[0] = -1;
+				transStringToNumber[1] = -1;
 			} else {
-				System.out.println(" 333");
-
-				final int i1 = Integer.parseInt(String.valueOf(args.charAt(i)));
-				if (transStringToNumber.length == 0) {
+				System.out.println("Start set value on stack");
+				int i1 = Integer.parseInt(String.valueOf(args.charAt(i)));
+				if (transStringToNumber[0] == transStringToNumber[1]) {
 					transStringToNumber[0] = i1;
 				} else {
 					transStringToNumber[1] = i1;
 				}
 			}
 		}
-
+		System.out.println(Arrays.toString(transStringToNumber));
 		System.out.println(result);
 	}
 
