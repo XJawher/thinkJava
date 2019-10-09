355840bcab7d0cb98829c99dff8c2310764d4a1d
咸菜 <lplee7830@gmail.com>
2019-10-09 19:49:06 +0800

    lesson five update data structure stack

--- helloworld/src/com/company/Main.java
+++ helloworld/src/com/company/Main.java
@@ -57,16 +57,10 @@ public class Main {
 */
 package com.company;
 
-import lesson.two.PostfixExpression;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * @author lipc
  */
-public class Main {
+/*public class Main {
 
     public static void main(String[] args) throws IOException {
         String readInput;
@@ -76,6 +70,58 @@ public class Main {
         pe.consoleLog("this is args");
         pe.getParameterFromMain(readInput);
     }
+}*/
+
+/*public class Main {
+	private static int a = 1;
+	private static int b = 2;
+
+	public static void main(String[] args) {
+		swap(a, b);
+		System.out.println("a is " + a + ", b is " + b);
+	}
+
+	private static void swap(int j, int i) {
+		a = i;
+		b = j;
+	}
+}*/
+
+/*public class Main {
+	public static void main(String args[]) {
+		int n = 10;
+		int t = fact(n);
+		System.out.println(t);
+	}
+
+	public static int fact(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * fact(n - 1);
+		}
+	}
+}*/
+
+public class Main {
+	public static void main(String args[]) {
+		A a = new A(1);
+		A b = new A(2);
+		swap(a, b);
+		System.out.println("a's value is " + a.value + ", b's value is " + b.value);
+	}
+
+	public static void swap(A a, A b) {
+		int t = a.value;
+		a.value = b.value;
+		b.value = t;
+	}
 }
 
+class A {
+	public int value;
 
+	public A(int v) {
+		this.value = v;
+	}
+}
